d4ae50cf6e6d1d41ae5fadb3ab549d1a809cc3a4
SoulToMind-PC\SoulToMind <soultomind930@gmail.com>
2025-08-02 16:18:10 +0900

    chore: 클래스 파일 추가

---
+++ ProcessExplorer/ProcessExplorer.Core/UIStringResources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessExplorer.Core
+{
+    public class UIStringResources
+    {
+        public const string MyGitHubUrl = "http://www.github.com/soultomind/ProcessExplorer";
+    }
+}
